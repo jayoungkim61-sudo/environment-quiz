4b49570a1e986cc97a440da028504bd2c0389ec9
jayoungkim61-sudo <jayoungkim61@gmail.com>
2025-10-23 15:11:55 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// ⚠️ 여기만 수정하세요! ⚠️
+// Firebase 콘솔에서 복사한 값을 아래에 붙여넣으세요
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVvHqKbwEjnzXZ7qP5Ev0NqofZhWS--hI",
+    authDomain: "environment-quiz-32303.firebaseapp.com",
+    databaseURL: "https://environment-quiz-32303-default-rtdb.firebaseio.com",
+    projectId: "environment-quiz-32303",
+    storageBucket: "environment-quiz-32303.firebasestorage.app",
+    messagingSenderId: "867653063103",
+    appId: "1:867653063103:web:898eaaffd281d6835f9d10a"
+};
+
+// 수정 끝! 다른 부분은 건드리지 마세요.
